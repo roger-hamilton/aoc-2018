f5de726569bba638b26e2beeb9a6a44d172f30a4
Roger Hamilton <roger.max.hamilton@gmail.com>
2018-12-09 15:42:32 -0600

    day 8 part 1

---
+++ day8/part1.js
@@ -0,0 +1,75 @@
+/**
+The sleigh is much easier to pull than you'd expect for something its weight. Unfortunately, neither you nor the Elves know which way the North Pole is from here.
+
+You check your wrist device for anything that might help. It seems to have some kind of navigation system! Activating the navigation system produces more bad news: "Failed to start navigation system. Could not read software license file."
+
+The navigation system's license file consists of a list of numbers (your puzzle input). The numbers define a data structure which, when processed, produces some kind of tree that can be used to calculate the license number.
+
+The tree is made up of nodes; a single, outermost node forms the tree's root, and it contains all other nodes in the tree (or contains nodes that contain nodes, and so on).
+
+Specifically, a node consists of:
+
+A header, which is always exactly two numbers:
+The quantity of child nodes.
+The quantity of metadata entries.
+Zero or more child nodes (as specified in the header).
+One or more metadata entries (as specified in the header).
+Each child node is itself a node that has its own header, child nodes, and metadata. For example:
+
+2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+A----------------------------------
+    B----------- C-----------
+                     D-----
+In this example, each node of the tree is also marked with an underline starting with a letter for easier identification. In it, there are four nodes:
+
+A, which has 2 child nodes (B, C) and 3 metadata entries (1, 1, 2).
+B, which has 0 child nodes and 3 metadata entries (10, 11, 12).
+C, which has 1 child node (D) and 1 metadata entry (2).
+D, which has 0 child nodes and 1 metadata entry (99).
+The first check done on the license file is to simply add up all of the metadata entries. In this example, that sum is 1+1+2+10+11+12+2+99=138.
+
+What is the sum of all metadata entries?
+ */
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const _ = require('lodash');
+
+const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf-8');
+
+const nums = input.split(' ').map(n => +n);
+
+const parseNode = list => {
+  const childCount = list[0];
+  const metaCount = list[1];
+  let idx = 2;
+  const children = [];
+  for(let i = 0; i < childCount; i++) {
+    const { node, consumed } = parseNode(list.slice(idx));
+    children.push(node);
+    idx += consumed;
+  }
+  const metadata = list.slice(idx, idx + metaCount);
+
+  return {
+    node: {
+      children,
+      metadata
+    },
+    consumed: idx + metaCount,
+  }
+};
+
+const sumMetadata = (root) => {
+  return _.sumBy(root.children, sumMetadata) + _.sum(root.metadata);
+}
+
+const test1 = `2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`.split(' ').map(n => +n);
+
+const test = parseNode(test1);
+
+assert(sumMetadata(test.node) === 138);
+
+const data = parseNode(nums);
+console.log(sumMetadata(data.node));
